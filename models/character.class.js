cfabdc2098d70c974b7a121726adce09f17e2198
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-08-09 15:40:45 +0200

    optimizing functionslength an clean code character.js

--- models/character.class.js
+++ models/character.class.js
@@ -128,59 +128,200 @@ class Character extends MovableObject {
    * @return {void}
    */
   animate() {
-    setInterval(() => this.moveCharacter(), 1000 / 60);
+    setStoppableInterval(() => this.moveCharacter(), 1000 / 60);
 
-    setStoppableInterval(() => this.playCharacter(),50);
+    setStoppableInterval(() => this.playCharacter(), 50);
   }
 
+  /**
+   * Moves the character if it is not dead.
+   *
+   * @return {void} This function does not return anything.
+   */
   moveCharacter() {
     if (!this.isDead()) {
-      if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-        this.moveRight();
-        this.otherDirection = false;
-        this.world.soundManager.playSound("run");
-      }
-
-      if (this.world.keyboard.LEFT && this.x > -360) {
-        this.moveLeft();
-        this.otherDirection = true;
-        this.world.soundManager.playSound("run");
-      }
-
-      if (this.world.keyboard.UP && !this.isAboveGround()) {
-        this.jump();
-        this.world.soundManager.playSound("jump");
-      }
-
+      if (this.canMoveRight()) this.moveCharRight();
+      if (this.canMoveLeft()) this.moveCharLeft();
+      if (this.canJump()) this.charJump();
       this.world.cameraX = -this.x + 100;
     }
   }
 
+  /**
+   * Checks if the character can move to the right.
+   *
+   * @return {boolean} True if the character can move to the right, false otherwise
+   */
+  canMoveRight() {
+    return this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x;
+  }
+
+  /**
+   * Moves the character to the right and updates the character's state.
+   *
+   * @return {void} This function does not return a value.
+   */
+  moveCharRight() {
+    this.moveRight();
+    this.otherDirection = false;
+    this.world.soundManager.playSound("run");
+  }
+
+  /**
+   * Determines if the character can move to the left.
+   *
+   * @return {boolean} Returns true if the character can move to the left, false otherwise.
+   */
+  canMoveLeft() {
+    return this.world.keyboard.LEFT && this.x > -360;
+  }
+
+  /**
+   * Moves the character to the left and updates the character's state.
+   *
+   * This function moves the character to the left by calling the `moveLeft` method of the character object.
+   * It also sets the `otherDirection` property of the character object to `true` to indicate that the character is moving in a direction other than right.
+   * Finally, it plays the "run" sound using the `playSound` method of the `world.soundManager` object.
+   *
+   * @return {void} This function does not return a value.
+   */
+  moveCharLeft() {
+    this.moveLeft();
+    this.otherDirection = true;
+    this.world.soundManager.playSound("run");
+  }
+
+  /**
+   * Determines if the character can jump based on keyboard input and position.
+   *
+   * @return {boolean} True if the character can jump, false otherwise.
+   */
+  canJump() {
+    return this.world.keyboard.UP && !this.isAboveGround();
+  }
+
+  /**
+   * Initiates a character jump action.
+   *
+   * @return {void} This function does not return a value.
+   */
+  charJump() {
+    this.jump();
+    this.world.soundManager.playSound("jump");
+  }
+
+  /**
+   * Plays the character animation based on the last move time and current state.
+   *
+   * This function determines the appropriate animation to play for the character based on the
+   * time elapsed since the last move and the current state of the character. If the character is
+   * dead, it checks if the death animation has been played and plays it if necessary. If the
+   * character is hurt, it plays the hurt animation. If the character is above ground, it plays
+   * the above ground animation. If the character is moving, it plays the walking animation.
+   * If the character is throwing, it plays the throwing animation. If it's time to sleep,
+   * it plays the sleeping animation. Otherwise, it plays the idle animation.
+   *
+   * @return {void} This function does not return a value.
+   */
   playCharacter() {
     const lastMove = Date.now() - this.lastMoveTime;
     const sleepTime = lastMove >= 3500;
-    if (this.isDead()) {
-      if (!this.playedDeathAnimation) {
-        this.world.soundManager.playSound("characterDeath");
-        this.playDeathAnimation();
-      }
-    } else if (this.isHurt()) {
-      this.playAnimation(this.IMAGES_HURT);
-      this.lastMoveTime = Date.now();
-    } else if (this.isAboveGround()) {
-      this.playAnimation(this.IMAGES_JUMPING);
-      this.lastMoveTime = Date.now();
-    } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-      this.playAnimation(this.IMAGES_WALKING);
-      this.lastMoveTime = Date.now();
-    } else if (this.world.keyboard.THROW) {
-      this.playAnimation(this.IMAGES_THROW);
-      this.lastMoveTime = Date.now();
-    } else if (sleepTime) {
-      this.playAnimation(this.IMAGES_SLEEPING);
-    } else {
+    if (this.charIsDead()) {
+      if (this.playedDeathAnimationCounter()) this.charDeathanimation();
+    } else if (this.isHurt()) this.charIsHurt();
+    else if (this.isAboveGround()) this.charIsAboveGround();
+    else if (this.charIsMoving()) this.charIsWalking();
+    else if (this.charIsThrowing()) this.throwingChar();
+    else if (sleepTime) this.playAnimation(this.IMAGES_SLEEPING);
+    else {
       this.playAnimation(this.IMAGES_IDLE);
     }
+  }
+
+  /**
+   * Returns a boolean indicating whether the death animation has been played or not.
+   *
+   * @return {boolean} True if the death animation has not been played, false otherwise.
+   */
+  playedDeathAnimationCounter() {
+    return !this.playedDeathAnimation;
+  }
+
+  /**
+   * Checks if the character is dead.
+   *
+   * @return {boolean} Returns true if the character is dead, false otherwise.
+   */
+  charIsDead() {
+    return this.isDead();
+  }
+
+  /**
+   * Plays the character death animation and sound effect.
+   *
+   * @return {void} This function does not return a value.
+   */
+  charDeathanimation() {
+    this.world.soundManager.playSound("characterDeath");
+    this.playDeathAnimation();
+  }
+
+  /**
+   * Plays the hurt animation for the character and updates the last move time.
+   *
+   * @return {void} This function does not return a value.
+   */
+  charIsHurt() {
+    this.playAnimation(this.IMAGES_HURT);
+    this.lastMoveTime = Date.now();
+  }
+
+  /**
+   * Checks if the character is above the ground.
+   *
+   * @return {void} This function does not return a value.
+   */
+  charIsAboveGround() {
+    this.playAnimation(this.IMAGES_JUMPING);
+    this.lastMoveTime = Date.now();
+  }
+
+  /**
+   * Checks if the character is currently moving.
+   *
+   * @return {boolean} True if the character is moving, false otherwise.
+   */
+  charIsMoving() {
+    return this.world.keyboard.RIGHT || this.world.keyboard.LEFT;
+  }
+
+  /**
+   * Plays the walking animation for the character and updates the last move time.
+   *
+   * @return {void} This function does not return a value.
+   */
+  charIsWalking() {
+    this.playAnimation(this.IMAGES_WALKING);
+    this.lastMoveTime = Date.now();
+  }
+
+  /**
+   * Checks if the character is currently throwing.
+   *
+   * @return {boolean} True if the character is throwing, false otherwise.
+   */
+  charIsThrowing() {
+    return this.world.keyboard.THROW;
+  }
+
+  /**
+   * Plays the throwing animation for the character and updates the last move time.
+   *
+   * @return {void} This function does not return a value.
+   */
+  throwingChar() {
+    this.playAnimation(this.IMAGES_THROW);
+    this.lastMoveTime = Date.now();
   }
 
   /**
