3dfd2f39f74dbafae7909d43d1ed47f951fe2922
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-08-09 14:21:46 +0200

    addind jsdoc game.js

--- js/game.js
+++ js/game.js
@@ -4,12 +4,21 @@ let keyboard = {};
 let intervalIds = [];
 let music = new Audio('audio/locoDinoTheme.mp3');
 
+
 document.addEventListener('DOMContentLoaded', () => {
     keyboard = new Keyboard();
     window.addEventListener('resize', checkScreenwidth);
     window.addEventListener('load', checkScreenwidth);
 });
 
+
+/**
+ * Initializes the game by setting up the canvas, creating a new World object,
+ * activating game buttons, closing instructions, checking screen width, and
+ * manipulating DOM elements.
+ *
+ * @return {void} This function does not return anything.
+ */
 function init() {
 canvas = document.getElementById('canvas');
 world = new World(canvas, keyboard);
@@ -20,21 +29,37 @@ document.getElementById('helpButton').classList.add('d-none');
 document.getElementById('restartButtonOverlay').classList.remove('d-none');
 document.getElementById('soundButton').classList.remove('d-none');
 document.getElementById('startButton').classList.add('d-none');
-
-console.log('my Character is' , world.character);
-console.log('my Enemies are' , world.enemies);
 }
 
+
+/**
+ * Starts the game by initializing the level with a delay.
+ *
+ * @return {void} This function does not return anything.
+ */
 function startGame() {
     initLevelWithDelay();
 }
 
+
+/**
+ * Initializes the level with a delay of 1000 milliseconds.
+ *
+ * @return {void} This function does not return a value.
+ */
 function initLevelWithDelay(){
     setTimeout(() => {
         initLevel();
     }, 1000);
 }
 
+
+/**
+ * Checks the current screen width and updates the visibility of the rotate device message, 
+ * rotate device image, and title accordingly.
+ *
+ * @return {void} This function does not return a value.
+ */
 function checkScreenwidth() {
     if (window.innerWidth <= 650) {
         document.getElementById("rotateDevice").classList.remove('d-none');
@@ -47,21 +72,46 @@ function checkScreenwidth() {
     }
  }
 
+
+ /**
+ * Activates the game buttons by removing the 'd-none' class from the 'gameButtons' element.
+ *
+ * @return {void} This function does not return a value.
+ */
  function activateGameButtons(){
      document.getElementById('gameButtons').classList.remove('d-none');
  }
 
+
+ /**
+ * Hides the start button, help button, and fullscreen button by adding the 'd-none' class to their respective elements.
+ *
+ * @return {void} This function does not return a value.
+ */
 function buttonsDisable() {
     document.getElementById('startButton').classList.add('d-none');
     document.getElementById('helpButton').classList.add('d-none');
     document.getElementById('fullscreenButton').classList.add('d-none');
 }
 
+
+/**
+ * Enters the fullscreen mode for the specified element.
+ *
+ * @param {HTMLElement} element - The element to enter fullscreen mode for.
+ * @return {void} This function does not return a value.
+ */
 function fullscreen() {
     let fullscreen = document.getElementById('fullscreen');
     enterFullscreen(fullscreen);
 }
 
+
+/**
+ * Toggles the fullscreen mode of the application.
+ *
+ * @return {void} This function does not return a value.
+ */
 function toggleFullscreen() {
     if (!document.fullscreenElement) {
         enterFullscreen();
@@ -70,6 +120,12 @@ function toggleFullscreen() {
     }
 }
 
+
+/**
+ * Enters the fullscreen mode for the element with the id 'fullscreen' and toggles the visibility of the fullscreen and exit fullscreen buttons.
+ *
+ * @return {void} This function does not return a value.
+ */
 function enterFullscreen() {
     const fullscreenElement = document.getElementById('fullscreen');
     if (fullscreenElement.requestFullscreen) {
@@ -81,11 +137,16 @@ function enterFullscreen() {
     } else if (fullscreenElement.msRequestFullscreen) { 
         fullscreenElement.msRequestFullscreen();
     }
-    
     document.getElementById('fullscreenButtonIMG').classList.add('d-none');
     document.getElementById('exitFullscreenButtonIMG').classList.remove('d-none');
 }
 
+
+/**
+ * Exits fullscreen mode if the browser supports it.
+ *
+ * @return {void} This function does not return anything.
+ */
 function exitFullscreen() {
     if (document.exitFullscreen) {
         document.exitFullscreen();
@@ -96,13 +157,16 @@ function exitFullscreen() {
     } else if (document.msExitFullscreen) { 
         document.msExitFullscreen();
     }
-  
     document.getElementById('fullscreenButtonIMG').classList.remove('d-none');
     document.getElementById('exitFullscreenButtonIMG').classList.add('d-none');
 }
 
 
-
+/**
+ * Opens the game instructions and buttons by removing the 'd-none' class.
+ *
+ * @return {void} This function does not return a value.
+ */
 function openInstructions() { 
     document.getElementById('gameInstructions').classList.remove('d-none');
     document.getElementById('instructions').classList.remove('d-none');
@@ -111,6 +175,12 @@ function openInstructions() {
 
 }
 
+
+/**
+ * Closes the game instructions and buttons by adding the 'd-none' class.
+ *
+ * @return {void} This function does not return a value.
+ */
 function closeInstructions() { 
     document.getElementById('gameInstructions').classList.add('d-none');
     document.getElementById('instructions').classList.add('d-none');
@@ -118,22 +188,33 @@ function closeInstructions() {
     document.getElementById('buttons').classList.remove('d-none');
 }
 
+
+/**
+ * Opens the keyboard settings and instructions by modifying the visibility of the corresponding elements.
+ *
+ * @return {void} This function does not return a value.
+ */
 function openkeyBoardSettings() {
     document.getElementById('keyboardInstructions').classList.remove('d-none');
     document.getElementById('homeButton').classList.add('d-none');
     document.getElementById('keyBoardButton').classList.add('d-none');
-    document.getElementById('backButton').classList.remove('d-none');
-    
+    document.getElementById('backButton').classList.remove('d-none'); 
 }
 
+
+/**
+ * Closes the keyboard settings and instructions by modifying the visibility of the corresponding elements.
+ *
+ * @return {void} This function does not return a value.
+ */
 function closeKeyBoardSettings() {
     document.getElementById('keyboardInstructions').classList.add('d-none');
     document.getElementById('homeButton').classList.remove('d-none');
     document.getElementById('keyBoardButton').classList.remove('d-none');
     document.getElementById('backButton').classList.add('d-none');
-
 }
     
+
 
 window.addEventListener('keydown', (event) => {
     if (event.keyCode == 39) {
@@ -158,6 +239,7 @@ window.addEventListener('keydown', (event) => {
     }
 });
 
+
 window.addEventListener('keyup', (event) => {
     if (event.keyCode == 39) {
         keyboard.RIGHT = false;
@@ -181,6 +263,12 @@ window.addEventListener('keyup', (event) => {
     }
 });
 
+
+/**
+ * Renders an image on a canvas element with the id 'canvas'.
+ *
+ * @return {void} This function does not return a value.
+ */
 function startScreen() {
     let ctx = document.getElementById('canvas').getContext('2d');
     let img = new Image;
@@ -190,37 +278,83 @@ function startScreen() {
     img.src = 'img/dino_background/2/background.png';
 }
     
+
+/**
+ * Creates a new interval that can be stopped and stores its ID.
+ *
+ * @param {function} fn - the function to be executed at the specified interval
+ * @param {number} time - the time interval in milliseconds
+ * @return {void} This function does not return a value.
+ */
 function setStoppableInterval(fn, time) {
     let id = setInterval(fn, time);
     intervalIds.push(id);
 }
 
+
+/**
+ * Displays the lost image and shows the restart button.
+ *
+ * @return {void} This function does not return a value.
+ */
 function lostImage() {
     document.getElementById('lostImage').classList.remove('d-none');
     showRestartButton();
 }
 
+
+/**
+ * Displays the won image and shows the restart button.
+ *
+ * @return {void} This function does not return a value.
+ */
 function wonImage() {
     document.getElementById('winImage').classList.remove('d-none');
     showRestartButton();
 }
 
+
+/**
+ * Stops all intervals stored in the intervalIds array.
+ *
+ * @return {void} This function does not return a value.
+ */
 function stopGame() {
     intervalIds.forEach(clearInterval);
 }
 
+
+/**
+ * Shows the restart button by removing the 'd-none' class from the 'restartButtonOverlay' element,
+ * adding the 'd-none' class to the 'startButton' element, and adding the 'd-none' class to the
+ * 'fullscreenButton' element.
+ *
+ * @return {void} This function does not return a value.
+ */
 function showRestartButton() {
     document.getElementById('restartButtonOverlay').classList.remove('d-none');
     document.getElementById('startButton').classList.add('d-none');
     document.getElementById('fullscreenButton').classList.add('d-none');
 }
 
+
+/**
+ * Plays the background music.
+ *
+ * @return {void}
+ */
 function playBackgroundmusic() {
     music.play();
     music.volume = 0.05;
     music.loop = 'loop';
 }
 
+
+/**
+ * Stops the background music.
+ *
+ * @return {void} This function does not return a value.
+ */
 function stopBackgroundmusic() {
     music.pause();
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,244 +1,237 @@
 class World {
-    character = new Character();
-    level = level1;
-    backgroundObjects = [];
-    canvas;
-    ctx;
-    keyboard;
-    cameraX = 0;
-    statusBar = new StatusBar();
-    bottlesBar = new BottlesBar();
-    collectedBottles = 0;
-    coinsBar = new CoinsBar();
-    collectedCoins = 0;
-    statusBarEndboss = new StatusBarEndboss();
-    endbossEnergy = 5;
-    throwableObjects = [];
-    soundManager = new Sounds();
-  
-    constructor(canvas, keyboard) {
-      this.ctx = canvas.getContext("2d");
-      this.canvas = canvas;
-      this.keyboard = keyboard;
-      this.generateBackgroundObjects();
-      this.draw();
-      this.setWorld();
-      this.run()
-    }
-  
-    setWorld() {
-      this.character.world = this;
-    }
+  character = new Character();
+  level = level1;
+  backgroundObjects = [];
+  canvas;
+  ctx;
+  keyboard;
+  cameraX = 0;
+  statusBar = new StatusBar();
+  bottlesBar = new BottlesBar();
+  collectedBottles = 0;
+  coinsBar = new CoinsBar();
+  collectedCoins = 0;
+  statusBarEndboss = new StatusBarEndboss();
+  endbossEnergy = 5;
+  throwableObjects = [];
+  soundManager = new Sounds();
 
-    run() {
-        setInterval(() => {
-              this.checkCollisions();
-              this.checkThrowObjects();
-        
-        }, 200);
-    }
+  constructor(canvas, keyboard) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.keyboard = keyboard;
+    this.generateBackgroundObjects();
+    this.draw();
+    this.setWorld();
+    this.run();
+  }
 
-    checkCollisions() {
-        setStoppableInterval(() => {
-          this.checkEnemieCollision();
-          this.checkBottleCollision();
-          this.checkCoinCollision();
-          this.checkBottleCollisionEnemy();
-          this.checkEndbossCollision();
-          this.checkEndbossHitCharacter();
-        }, 40);
+  setWorld() {
+    this.character.world = this;
+  }
 
-            
-    }
+  run() {
+    setInterval(() => {
+      this.checkCollisions();
+      this.checkThrowObjects();
+    }, 200);
+  }
 
-    checkEndbossHitCharacter() {
-        this.level.endboss.forEach((endboss) => {
-            if (this.character.isColliding(endboss)) {
-                this.character.hit();
-                this.soundManager.playSound('hurt');
-                this.statusBar.setPercentage(this.character.energy);
-            }
-        })
-    }
+  checkCollisions() {
+    setStoppableInterval(() => {
+      this.checkEnemieCollision();
+      this.checkBottleCollision();
+      this.checkCoinCollision();
+      this.checkBottleCollisionEnemy();
+      this.checkEndbossCollision();
+      this.checkEndbossHitCharacter();
+    }, 40);
+  }
 
-    checkEndbossCollision() {
-        if (this.collectedBottles > 0) {
-          this.throwableObjects = this.throwableObjects.filter((bottle) => {
-            if (this.level.endboss[0].isColliding(bottle)) {
-              this.soundManager.playSound('endboss');
-              this.soundManager.playSound('glass');
-              this.endbossEnergy--;
-              this.level.endboss[0].hit();
-              this.statusBarEndboss.setPercentage(this.endbossEnergy);
-              return false;
-              }
-              else {
-                return true;
-              }
-          });
-        }
+  checkEndbossHitCharacter() {
+    this.level.endboss.forEach((endboss) => {
+      if (this.character.isColliding(endboss)) {
+        this.character.hit();
+        this.soundManager.playSound("hurt");
+        this.statusBar.setPercentage(this.character.energy);
       }
+    });
+  }
 
-    checkBottleCollisionEnemy() {
-      this.throwableObjects.forEach((bottle, index) => {
-        this.level.enemies.forEach((enemy) => {
-          if (bottle.isColliding(enemy)) {
-            this.soundManager.playSound('glass');
-            enemy.hit();
-            
-          }
-        })
-      })
-    }
-
-    checkCoinCollision() {
-        this.level.coins = this.level.coins.filter((coin) => {
-            if (this.character.isColliding(coin)) {
-              this.soundManager.playSound('coin');
-              this.collectedCoins++;
-              this.coinsBar.setPercentage(this.collectedCoins);
-              return false;
-            }
-              return true;
-            });
+  checkEndbossCollision() {
+    if (this.collectedBottles > 0) {
+      this.throwableObjects = this.throwableObjects.filter((bottle) => {
+        if (this.level.endboss[0].isColliding(bottle)) {
+          this.soundManager.playSound("endboss");
+          this.soundManager.playSound("glass");
+          this.endbossEnergy--;
+          this.level.endboss[0].hit();
+          this.statusBarEndboss.setPercentage(this.endbossEnergy);
+          return false;
+        } else {
+          return true;
         }
-
-    checkBottleCollision() {
-        this.level.bottles = this.level.bottles.filter((bottle) => {
-            if (this.character.isColliding(bottle)) {
-              this.soundManager.playSound('bottle');
-              this.collectedBottles++;
-              this.bottlesBar.setPercentage(this.collectedBottles);
-              return false;
-            }
-              return true;
-            });
-        }
-
-    checkEnemieCollision() {
-      this.level.enemies.forEach((enemy, index) => {
-        if (!enemy.isDead() && this.character.isColliding(enemy)) {
-          if (this.character.isAboveGround() && !this.character.isHurt()) {
-            this.character.jump(10);
-            enemy.hit();
-            setTimeout(() => {
-              this.level.enemies.splice(index, 1);
-            }, 500);
-          } else {
-            this.character.hit();
-            this.soundManager.playSound('hurt');
-            this.statusBar.setPercentage(this.character.energy);
-            } 
-          }
-        }
-      );
-    }
-        
-
-    checkThrowObjects() {
-          if (this.keyboard.THROW && this.collectedBottles > 0 && this.character.otherDirection == false) {     
-            let bottle = new ThrowableObject (this.character.x +100, this.character.y +90);
-            this.throwableObjects.push(bottle);
-            this.collectedBottles--;
-            this.bottlesBar.setPercentage(this.collectedBottles);
-            }
-        
-        }
-  
-    generateBackgroundObjects() {
-        const layersBlock1 = [
-          "img/dino_background/4/1.png",
-          "img/dino_background/4/2.png",
-          "img/dino_background/4/3.png",
-          "img/dino_background/4/4.png",
-          "img/dino_background/4/5.png",
-        ];
-    
-        const layersBlock2 = [
-            "img/dino_background/4/1.png",
-            "img/dino_background/4/2.png",
-            "img/dino_background/4/3.png",
-            "img/dino_background/4/4.png",
-            "img/dino_background/4/5.png"
-        ];
-    
-        for (let i = -1; i < 5; i++) {
-          let offset = i * 719;
-          let layers = i % 2 === 0 ? layersBlock1 : layersBlock2;
-          for (let j = 0; j < layers.length; j++) {
-            this.backgroundObjects.push(new BackgroundObject(layers[j], offset));
-          }
-        }
-      }
-    
-  
-    draw() {
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  
-      this.ctx.translate(this.cameraX, 0);
-  
-      this.addObjectsToMap(this.backgroundObjects);
-      this.addObjectsToMap(this.level.clouds);
-      this.addObjectsToMap(this.level.tropeognathus);
-
-      this.ctx.translate(-this.cameraX, 0);
-      // ----- Space for fixed Objects -----
-      this.addToMap(this.statusBar);
-      this.addToMap(this.bottlesBar);
-      this.addToMap(this.coinsBar);
-      this.addToMap(this.statusBarEndboss);
-      this.ctx.translate(this.cameraX, 0);
-        
-      this.addObjectsToMap(this.throwableObjects);
-      
-      this.addToMap(this.character);
-
-      
-      this.addObjectsToMap(this.level.enemies);
-      this.addObjectsToMap(this.level.endboss);
-      this.addObjectsToMap(this.level.bottles);
-      this.addObjectsToMap(this.level.coins);
-      this.ctx.translate(-this.cameraX, 0);
-  
-      let self = this;
-      requestAnimationFrame(function () {
-      self.draw();
       });
     }
-  
-    addObjectsToMap(objects) {
-      objects.forEach((object) => {
-        this.addToMap(object);
+  }
+
+  checkBottleCollisionEnemy() {
+    this.throwableObjects.forEach((bottle, index) => {
+      this.level.enemies.forEach((enemy) => {
+        if (bottle.isColliding(enemy)) {
+          this.soundManager.playSound("glass");
+          enemy.hit();
+        }
       });
-    }
-  
-    addToMap(movableObject) {
-      if (movableObject.otherDirection) {
-        this.flipImage(movableObject);
+    });
+  }
+
+  checkCoinCollision() {
+    this.level.coins = this.level.coins.filter((coin) => {
+      if (this.character.isColliding(coin)) {
+        this.soundManager.playSound("coin");
+        this.collectedCoins++;
+        this.coinsBar.setPercentage(this.collectedCoins);
+        return false;
       }
+      return true;
+    });
+  }
 
-        movableObject.draw(this.ctx);
-        movableObject.drawBorder(this.ctx);
-  
-      if (movableObject.otherDirection) {
-        this.flipImageBack(movableObject);
+  checkBottleCollision() {
+    this.level.bottles = this.level.bottles.filter((bottle) => {
+      if (this.character.isColliding(bottle)) {
+        this.soundManager.playSound("bottle");
+        this.collectedBottles++;
+        this.bottlesBar.setPercentage(this.collectedBottles);
+        return false;
       }
-    }
+      return true;
+    });
+  }
 
-
-    flipImage(movableObject) {
-        this.ctx.save();
-        this.ctx.translate(movableObject.width, 0);
-        this.ctx.scale(-1, 1);
-        movableObject.x = movableObject.x * -1;
-    }
-
-
-    flipImageBack(movableObject) {
-        movableObject.x = movableObject.x * -1;
-        this.ctx.restore();
-    }
-
+  checkEnemieCollision() {
+    this.level.enemies.forEach((enemy, index) => {
+      if (!enemy.isDead() && this.character.isColliding(enemy)) {
+        if (this.character.isAboveGround() && !this.character.isHurt()) {
+          this.character.jump(10);
+          enemy.hit();
+          setTimeout(() => {
+            this.level.enemies.splice(index, 1);
+          }, 500);
+        } else {
+          this.character.hit();
+          this.soundManager.playSound("hurt");
+          this.statusBar.setPercentage(this.character.energy);
+        }
+      }
+    });
   }
   
-  
+
+  checkThrowObjects() {
+    if (
+      this.keyboard.THROW &&
+      this.collectedBottles > 0 &&
+      this.character.otherDirection == false
+    ) {
+      let bottle = new ThrowableObject(
+        this.character.x + 100,
+        this.character.y + 90
+      );
+      this.throwableObjects.push(bottle);
+      this.collectedBottles--;
+      this.bottlesBar.setPercentage(this.collectedBottles);
+    }
+  }
+
+  generateBackgroundObjects() {
+    const layersBlock1 = [
+      "img/dino_background/4/1.png",
+      "img/dino_background/4/2.png",
+      "img/dino_background/4/3.png",
+      "img/dino_background/4/4.png",
+      "img/dino_background/4/5.png",
+    ];
+
+    const layersBlock2 = [
+      "img/dino_background/4/1.png",
+      "img/dino_background/4/2.png",
+      "img/dino_background/4/3.png",
+      "img/dino_background/4/4.png",
+      "img/dino_background/4/5.png",
+    ];
+
+    for (let i = -1; i < 5; i++) {
+      let offset = i * 719;
+      let layers = i % 2 === 0 ? layersBlock1 : layersBlock2;
+      for (let j = 0; j < layers.length; j++) {
+        this.backgroundObjects.push(new BackgroundObject(layers[j], offset));
+      }
+    }
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.ctx.translate(this.cameraX, 0);
+
+    this.addObjectsToMap(this.backgroundObjects);
+    this.addObjectsToMap(this.level.clouds);
+    this.addObjectsToMap(this.level.tropeognathus);
+
+    this.ctx.translate(-this.cameraX, 0);
+    // ----- Space for fixed Objects -----
+    this.addToMap(this.statusBar);
+    this.addToMap(this.bottlesBar);
+    this.addToMap(this.coinsBar);
+    this.addToMap(this.statusBarEndboss);
+    this.ctx.translate(this.cameraX, 0);
+
+    this.addObjectsToMap(this.throwableObjects);
+
+    this.addToMap(this.character);
+
+    this.addObjectsToMap(this.level.enemies);
+    this.addObjectsToMap(this.level.endboss);
+    this.addObjectsToMap(this.level.bottles);
+    this.addObjectsToMap(this.level.coins);
+    this.ctx.translate(-this.cameraX, 0);
+
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach((object) => {
+      this.addToMap(object);
+    });
+  }
+
+  addToMap(movableObject) {
+    if (movableObject.otherDirection) {
+      this.flipImage(movableObject);
+    }
+
+    movableObject.draw(this.ctx);
+    movableObject.drawBorder(this.ctx);
+
+    if (movableObject.otherDirection) {
+      this.flipImageBack(movableObject);
+    }
+  }
+
+  flipImage(movableObject) {
+    this.ctx.save();
+    this.ctx.translate(movableObject.width, 0);
+    this.ctx.scale(-1, 1);
+    movableObject.x = movableObject.x * -1;
+  }
+
+  flipImageBack(movableObject) {
+    movableObject.x = movableObject.x * -1;
+    this.ctx.restore();
+  }
+}
